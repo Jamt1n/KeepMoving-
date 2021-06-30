b7d024cfa003fcbdaa2e3a9145c54bbc7fc77d55
lujunting <1316560980@qq.com>
2021-07-01 00:33:10 +0800

    feat:王者荣耀作业

--- 面向对象/王者荣耀模块化练习/game/heroes/yase.js
+++ 面向对象/王者荣耀模块化练习/game/heroes/yase.js
@@ -2,10 +2,15 @@ import S1 from '../skills/yase/s1.js';
 import S2 from '../skills/yase/s2.js';
 import S3 from '../skills/yase/s3.js';
 
+import Skin1 from "../skins/yase/skin1.js";
+import Skin2 from "../skins/yase/skin2.js";
+import Skin3 from "../skins/yase/skin3.js";
+
 export default class Yase {
     constructor(){
         this.name = "亚瑟";
         this.ico = "./sources/heroes/yase1.png";
         this.skills = [new S1(),new S2(),new S3()];
+        this.skins = [new Skin1(),new Skin2(),new Skin3()];
     }
 }
