5ec2da263da9ddfde7eca23597654d47f9608116
lujunting <1316560980@qq.com>
2021-07-05 23:35:25 +0800

    feat:MyjQuery--css方法

--- 面向对象-MyjQuery/myjq.js
+++ 面向对象-MyjQuery/myjq.js
@@ -7,7 +7,12 @@
 // }
 
 class Jq {
-  constructor(arg) {
+  constructor(arg, root) {
+    if (typeof root === "undefined") {
+      this["prevObject"] = [document];
+    } else {
+      this["prevObject"] = root;
+    }
     if (typeof arg === "string") {
       // this.elm = document.querySelector(arg);
       // this[0] = elm1 ....
@@ -37,13 +42,49 @@ class Jq {
     }
   }
   on(eventName, cb) {
-      let eventArr = eventName.split(' ');
-      // 针对多个节点绑定多个事件
-      for (let i=0;i<this.length; i++) {
-          for (let j = 0;j<eventArr.length;j++) {
-              this[i].addEventListener(eventArr[j], cb);
-          }
+    let eventArr = eventName.split(" ");
+    // 针对多个节点绑定多个事件
+    for (let i = 0; i < this.length; i++) {
+      for (let j = 0; j < eventArr.length; j++) {
+        this[i].addEventListener(eventArr[j], cb);
       }
+    }
+  }
+  eq(index) {
+    return new Jq(this[index], this);
+  }
+  end() {
+    return this["prevObject"];
+  }
+  get(index) {
+    return this[index];
+  }
+  css(...args) {
+    if (args.length === 1) {
+      if (typeof args[0] === "object") {
+        // 1 多样式
+        for (let i = 0; i < this.length; i++) {
+          for (let j in args[0]) {
+            this.#setStyle(this[i], j, args[0][j]);
+          }
+        }
+      } else {
+        // 2 一个样式 ：多个元素只会获取第一个元素样式
+        return this.#getStyle(this[0], args[0]);
+      }
+    } else {
+      // 3 两个参数， 设置一个样式
+      for (let i = 0; i < this.length; i++) {
+        this.#setStyle(this[0], args[0], args[1]);
+      }
+    }
+    return this;
+  }
+  #getStyle(ele, styleName) {
+    return getComputedStyle(ele, null)[styleName];
+  }
+  #setStyle(ele, styleName, styleValue) {
+    ele.style[styleName] = styleValue;
   }
 }
 
