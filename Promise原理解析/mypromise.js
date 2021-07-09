1ee2e063aacb33d47078299449372d396e96fd2e
lujt8687 <lujt8687@fxiaoke.com>
2021-07-09 11:29:05 +0800

    feat:作业 finally 和 all

--- Promise原理解析/mypromise.js
+++ Promise原理解析/mypromise.js
@@ -60,4 +60,96 @@ export default class MyPromise {
     //   onRejected && onRejected(this["[[PromiseResult]]"]);
     // }
   }
+
+  catch(fn) {
+    return this.then(undefined, fn);
+  }
+  static resolve(val) {
+    return new MyPromise((resolve) => {
+      resolve(val);
+    });
+  }
+  static reject(err) {
+    return new MyPromise((resolve, reject) => {
+      reject(err);
+    });
+  }
+  static race(lists) {
+    let isExe = false;
+    return new MyPromise((resolve, reject) => {
+      lists.forEach((item) => {
+        item.then(
+          (res) => {
+            if (!isExe) {
+              resolve(res);
+              isExe = true;
+            }
+          },
+          (err) => {
+            if (!isExe) {
+              reject(err);
+              isExe = true;
+            }
+          }
+        );
+      });
+    });
+  }
+  static allSettled(lists) {
+    // let resArr = [1,2];
+    let resArr = new Array(lists.length);
+    let num = 0;
+    return new MyPromise((reslove) => {
+      lists.forEach((item, key) => {
+        let obj = {};
+        item.then(
+          (res) => {
+            obj["status"] = "fulfilled";
+            obj["value"] = res;
+            resArr[key] = obj;
+            // console.log(1,resArr);
+            num++;
+            if (num >= lists.length) {
+              reslove(resArr);
+            }
+          },
+          (err) => {
+            obj["status"] = "rejected";
+            obj["reson"] = err;
+            resArr[key] = obj;
+            num++;
+            // console.log(2,resArr);
+            if (num >= lists.length) {
+              reslove(resArr);
+            }
+          }
+        );
+      });
+    });
+  }
+  // 作业
+  finally(fn) {
+    //无论结果是fulfilled或者是rejected，都会执行指定的回调函数
+    return this.then(
+      (res) => MyPromise.resolve(fn()).then(() => res),
+      (err) => MyPromise.resolve(fn()).then(() => { //用resolve因为最终用的是throw抛出错误
+          throw err;
+        })
+    );
+  }
+  static all(lists) {
+    let resArr = new Array(lists.length);
+    let num = 0;
+    return new MyPromise((reslove) => {
+      lists.forEach((item, key) => {
+        item.then((res) => {
+          resArr[key] = res;
+          num++;
+          if (num >= lists.length) {
+            reslove(resArr); // 只返回resolve值的数组
+          }
+        });
+      });
+    });
+  }
 }
