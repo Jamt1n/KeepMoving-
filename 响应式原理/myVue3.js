768ac6a14712c01795b453796da5e2a35f2d9109
lujunting <1316560980@qq.com>
2021-07-11 23:30:25 +0800

    refactor:myVue3--Proxy改造

---
+++ 响应式原理/myVue3.js
@@ -0,0 +1,95 @@
+/**
+ * Proxy重构
+ */
+class myVue {
+  constructor(options) {
+    this.$options = options;
+    this._data = options.data;
+    this.observe(this._data);
+    this.compile();
+  }
+  compile() {
+    let ele = document.querySelector(this.$options.el);
+    this.compileNodes(ele);
+  }
+  compileNodes(ele) {
+    let childNodes = ele.childNodes;
+    childNodes.forEach((node) => {
+      if (node.nodeType === 1) {
+        // 元素节点
+        if (node.childNodes.length > 0) {
+          this.compileNodes(node);
+        }
+      } else if (node.nodeType === 3) {
+        // 文本
+        let reg = /\{\{\s*([^{}\s]+)\s*\}\}/g;
+        let textContent = node.textContent;
+        // console.log(textContent)
+        if (reg.test(textContent)) {
+          let $1 = RegExp.$1;
+          // console.log("有大胡子", $1, this._data[$1]);
+          node.textContent = node.textContent.replace(reg, this._data[$1]);
+
+          // 事件监听
+          // this.addEventListener($1, (e) => {
+          //   console.log("视图更新");
+          //   let oldValue = this._data[$1];
+          //   let newValue = e.detail;
+          //   node.textContent = node.textContent.replace(oldValue, newValue);
+          // });
+          new Watcher(this._data, $1, (newValue) => {
+            console.log("视图更新");
+            let oldValue = this._data[$1];
+            node.textContent = node.textContent.replace(oldValue, newValue);
+          });
+        }
+      }
+    });
+  }
+  //  -------- refactor
+  observe(data) {
+    let temp = {};
+
+    this._data = new Proxy(data, {
+      get(target, key) {
+        console.log("get");
+        temp[key] = new Dep();
+        if (Dep.target) {
+          temp[key].addSub(Dep.target);
+        }
+        return Reflect.get(target, key);
+      },
+      set(target, key, newValue) {
+        console.log("set");
+        temp[key].notify(newValue);
+        return Reflect.set(target, key, newValue);
+      },
+    });
+  }
+}
+
+class Dep {
+  constructor() {
+    this.subs = [];
+  }
+  addSub(sub) {
+    this.subs.push(sub);
+  }
+  notify(newValue) {
+    this.subs.forEach((sub) => {
+      sub.update(newValue);
+    });
+  }
+}
+
+class Watcher {
+  constructor(data, key, cb) {
+    Dep.target = this;
+    data[key]; // 触发get 收集watcher
+    this.cb = cb;
+    Dep.target = null;
+  }
+  update(newValue) {
+    this.cb(newValue);
+  }
+}
