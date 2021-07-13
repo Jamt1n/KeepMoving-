a5e0c4107936e5670ca0a96d99d23b51fd222f78
lujunting <1316560980@qq.com>
2021-07-13 23:44:26 +0800

    feat: 引入fs、url模块

--- Node/app/app2.js
+++ Node/app/app2.js
@@ -1,8 +1,6 @@
 // 使用 Node.js 内置 require 方法引入核心模块 -> http
 const http = require('http');
 
-const url = require('url');
-
 // fs模块
 const fs = require('fs');
 
@@ -19,21 +17,16 @@ const server = http.createServer();
 
 // 注册 request 事件回调函数，当有客户端连接请求被监听到的时候执行回调
 server.on('request', (req, res) => {
+    console.log(`有客户端请求`);
 
     // 获取请求相关信息
     // 当前请求的 url 字符串
     console.log(req.url);
 
-    // 使用 Node.js 的 url 模块中提供的工具方法解析 url 字符串
-    const urlObj = url.parse(req.url);
-    console.log(urlObj);
-
-    try {
-        let content = fs.readFileSync(`./resources${urlObj.pathname}`).toString();
-        res.end(content);
-    } catch (e) {
-        res.end(Math.random()+'');
-    }
+    // 写入数据
+    res.write('Hello');
+    // 结束数据写入
+    res.end();
 });
 
 // 指定当前 Server 需要监听的主机
