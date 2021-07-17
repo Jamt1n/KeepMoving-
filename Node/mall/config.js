a3b0daa8008846627eb89c79982a2a3636342769
lujunting <1316560980@qq.com>
2021-07-17 17:09:32 +0800

    init:Koa

---
+++ Node/mall/config.js
@@ -0,0 +1,24 @@
+module.exports = {
+  server: {
+    port: 8888,
+  },
+  static: {
+    prefix: "/public",
+    dir: "./public",
+    gzip: true,
+    dynamic: true,
+  },
+  database: {
+    host: "localhost",
+    port: 3306,
+    user: "root",
+    password: "123456",
+    database: "mall",
+  },
+  template: {
+    dir: "./template",
+  },
+  user: {
+    cookieSignKey: ["mall"],
+  },
+};
