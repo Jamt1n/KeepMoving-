62b511fef2c2b1d60118737259b983d6c865774d
Jamtin <1316560980@qq.com>
2021-07-21 00:31:41 +0800

    fix:bind

--- MyUtils/bind.js
+++ MyUtils/bind.js
@@ -2,6 +2,17 @@ function bind(Fn, obj, ...args) {
   // 返回一个函数
   return function (...args2) {
     // 执行 call 函数
-    return call(Fn, obj, ...args, ...args2);
+    return Fn.call(obj, ...args, ...args2);
   };
 }
+
+let b = {
+  name: "jam",
+};
+let a = {
+  fn: function () {
+    console.log(this.name);
+  },
+};
+
+bind(a.fn, b)();
