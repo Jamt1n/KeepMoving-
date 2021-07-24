cf3b5b2047bc9fb8e4a288508f6473a851612491
Jamtin <1316560980@qq.com>
2021-07-24 15:53:33 +0800

    update: _flat

--- MyUtils/flat.js
+++ MyUtils/flat.js
@@ -1,14 +1,28 @@
-function flat(arr) {
-    let res = [];
-    arr.forEach(item => {
-        if (Array.isArray(item)) {
-            res = res.concat(flat(item));
-        }else {
-            res.push(item);
+// function flat(arr) {
+//     let res = [];
+//     arr.forEach(item => {
+//         if (Array.isArray(item)) {
+//             res = res.concat(flat(item));
+//         }else {
+//             res.push(item);
+//         }
+//     })
+//     return res;
+// }
+
+Array.prototype._flat = function(n) {
+    n = n === undefined ? 1 : n;
+    if (n < 1) return this;
+    let newArr = [];
+    for (let i = 0; i < this.length; i++) {
+        if (Array.isArray(this[i])) {
+            newArr = newArr.concat(this[i].flat(n - 1));
+        } else {
+            newArr.push(this[i]);
         }
-    })
-    return res;
+    }
+    return newArr;
 }
 
 let arr = [1,3,2,[5,[4]]];
-console.log(flat(arr));
+console.log(arr._flat());
