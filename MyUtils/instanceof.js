88f9e77b4dce310583b2d9c999c87970f8af5e55
Jamtin <1316560980@qq.com>
2021-07-30 13:02:44 +0800

    feat:instanceof

---
+++ MyUtils/instanceof.js
@@ -0,0 +1,9 @@
+function _instanceof(target, origin) {
+    while(target) {
+        if (target.__proto__ === origin.prototype) {
+            return true;
+        }
+        target = target.__proto__;
+    }
+    return false;
+}
