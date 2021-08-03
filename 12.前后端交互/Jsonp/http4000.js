ea21b10d10ecf3ad99ea174bc6695311aa411ac4
Jamtin <1316560980@qq.com>
2021-08-03 23:45:04 +0800

    feat: 跨域情况

---
+++ 12.前后端交互/Jsonp/http4000.js
@@ -0,0 +1,21 @@
+const Koa = require('koa');
+const static = require('koa-static');
+const Router = require('koa-router');
+
+const app = new Koa();
+let router = new Router();
+
+app.use(static(__dirname+'/static'));
+router.get('/', (ctx, next) => {
+    ctx.body = "hello run at 4000";
+})
+
+router.get('/getAjax', (ctx, next) => {
+    ctx.body = {
+        name: 'jam4000',
+        age: 20
+    };
+})
+
+app.use(router.routes());
+app.listen(4000)
