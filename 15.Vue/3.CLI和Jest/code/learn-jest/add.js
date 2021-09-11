444f3eb148d620195cb71eb2a77a45833fbb53ab
Jamt1n <51704421+Jamt1n@users.noreply.github.com>
2021-09-12 01:32:53 +0800

    Delete 15.Vue/3.CLI和Jest directory
    fix

--- 15.Vue/3.CLI和Jest/code/learn-jest/add.js
+++
@@ -1,6 +0,0 @@
-// 1. input (a,b)
-// 2. output -> value
-
-module.exports = (a, b) => {
-  return a + b;
-};
--- 15.Vue/3.CLI和Jest/code/learn-jest/index.js
+++
@@ -1,6 +0,0 @@
-// const add = require("./add.js");
-
-// const a = 1;
-// const b = 1;
-// const r = add(a, b);
-// console.log(r);
